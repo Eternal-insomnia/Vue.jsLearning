39b26108d946bef68eb49919fc57376e1b94514c
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-02 20:53:01 +0300

    Added example for event listeres
    Added example with comms for event listeres in EventListener.vue

---
+++ EventListeners.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() { // some reactive data
+    return {
+      count: 0
+    }
+  },
+  methods: { // like functions but methods
+    increment() {
+      this.count++; // to access 'data' variables use this.
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- сделайте так, чтобы кнопка работала -->
+  <button v-on:click="increment">Количество: {{ count }}</button>
+</template>
