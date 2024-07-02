68a99a45e8bf51c77728cebc482646be7ffc6138
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-02 16:47:44 +0300

    Create Declarative_Rendering.vue
    + Declarative_Rendering.vue

---
+++ Declarative_Rendering.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue'
+
+  const message = ref('ыые') // works with any value types
+  message.value = 'Изменено' // needs .value
+  
+  const counter = reactive({ // only works on objects (including arrays and built-in types such as Map & Set)
+    count: 0
+  })
+  counter.count++ // don't need .value
+</script>
+
+<template>
+  <h1>Сделай меня динамическим!</h1>
+  <h1>{{message}}</h1>
+  <p>Количество: {{counter.count}}</p>
+  <p>{{message.split('').reverse().join('')}}</p>
+</template>
