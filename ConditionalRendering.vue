713ebf53355057aa74a1ec93d67025ac1578a26b
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-03 11:21:07 +0300

    Added example for Conditional rendering
    Added example with comms for conditiolan rendering in ConditionalRendering.vue

---
+++ ConditionalRendering.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.awesome = !this.awesome // switches condition
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- switching text with using directives v-if and v-else-->
+  <button @click="toggle">Переключить</button>
+  <h1 v-if="awesome">Vue - это потрясающе!</h1>
+  <h1 v-else>О нет 😢</h1>
+</template>
