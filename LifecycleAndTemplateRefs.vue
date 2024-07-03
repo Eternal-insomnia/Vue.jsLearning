949f2ab5b6d3206af1e0795dcf172869e8e25c52
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-03 22:04:45 +0300

    Added example for lyfecycle and template refs
    Added example with comms for lyfecycle and template refs in LyfecycleAndTemplateRefs.vue

---
+++ LifecycleAndTemplateRefs.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  mounted() { // using his hook to get access to an element
+    this.$refs.pElementRef.textContent = "balls.ыые"
+  }
+}
+</script>
+
+<template>
+  <p ref="pElementRef">Привет</p> <!-- template ref (reference to an element) -->
+</template>
