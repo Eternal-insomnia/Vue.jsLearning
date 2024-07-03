861652c42711b3bc4ad909db0b4c77b1933cbae1
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-03 10:43:22 +0300

    Added example for form binding
    Added example with comms for form bindings in FormBindings.vue

---
+++ FormBinding.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+      text1: ''
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.text = e.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <input :value="text" @input="onInput" placeholder="Пишите тут"> <!-- we can use v-bind and v-on together to change variable -->
+  <p>{{ text }}</p>
+  <input v-model="text1" placeholder="Так же, только круче"> <!-- same but shorter -->
+	<p>{{ text1 }}</p>
+</template>
