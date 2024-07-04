5c1e4838226d5e35c481d1cea968128c72f7b676
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-04 16:23:05 +0300

    Added example for props
    Added folder with example and comms for props.

---
+++ Props/App.vue
@@ -0,0 +1,18 @@
+<script>
+import ChildComp from './ChildComp.vue' // import component
+
+export default {
+  components: {
+    ChildComp // registrating component
+  },
+  data() {
+    return {
+      greeting: 'Привет от родителя'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp :msg="greeting" /> <!-- giving property to msg -->
+</template>
---
+++ Props/ChildComp.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: {
+    msg: String // prop
+  }
+}
+</script>
+
+<template>
+  <h2>{{ msg || 'пока входные параметры не переданы' }}</h2> <!-- this is displayed on the page -->
+</template>
