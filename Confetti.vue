b4301c5fc419d223f7e3cf5ff71168f0b66ac3ca
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-04 18:28:05 +0300

    Created Confetti.vue
    Just confetti

---
+++ Confetti.vue
@@ -0,0 +1,19 @@
+<script>
+import JSConfetti from 'js-confetti'
+const confetti = new JSConfetti()
+
+export default {
+    mounted() {
+        this.showConfetti()
+    },
+    methods: {
+        showConfetti() {
+            confetti.addConfetti()
+        }
+    }
+}
+</script>
+
+<template>
+    <h1 @click="showConfetti">🎉 Поздравляем!</h1>
+</template>
