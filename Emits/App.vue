7b03c0e3493ee0e8424cb38ec0379809a6a01b5c
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-04 16:49:01 +0300

    Added an example of emits in vue
    In Emits folder

---
+++ Emits/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ChildComp from './ChildComp.vue'; // importing the component
+export default {
+    components: {
+        ChildComp // registrating the component
+    },
+    data () {
+        return {
+            childMsg: "Ничего, совсем ничего..."
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- using the v-on directive, we listen for the event of the child component -->
+    <ChildComp @response="(msg) => childMsg = msg"/> <!-- getting an additional argument -->
+    <p>{{ childMsg }}</p>
+</template>
---
+++ Emits/ChildComp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    // announcement of generated events
+    emits: ['response'],
+    created() {
+        // generating event with parameters (1st - name, 2nd and another - additional)
+        this.$emit('response', 'саламчик от дочернего компонента')
+    }
+}
+</script>
+
+<template>
+    <p>Дочерний компонент</p>
+</template>
