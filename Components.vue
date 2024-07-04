5705513e958dc99798d9ec201b3ae56f946f4cc8
Eternal-insomnia <alexsuvorov2506@gmail.com>
2024-07-04 13:34:22 +0300

    Added example for components
    Added example with comms for components in Components.vue

---
+++ Components.vue
@@ -0,0 +1,13 @@
+<script>
+import ChildComp from './ChildComp.vue' // importing the component
+
+export default {
+  components: { // register the component
+    ChildComp
+  }
+}
+</script>
+
+<template>
+  <ChildComp /> <!-- using the component-->
+</template>
